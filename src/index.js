b3dbc78c1b3d8c6529ad7f8d594f6b83aaccd1d0
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-02-16 16:37:56 +0530

    Added code for created a new project on the backend  | add project creation feature with UUID and API endpoints

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import express from "express";
 import cors from "cors";
 
+import apiRouter from "./routes/index.js";
+
 import { PORT } from "./config/serverConfig.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use("/api", apiRouter);
 
 app.get("/ping", (req, res) => {
   return res.json({ message: "pong" });
