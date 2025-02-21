f12ad84f89657c1f1b1ccdd915f5db9398e24f29
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-02-21 21:32:45 +0530

    Added folder structure in backend

--- src/routes/v1/projects.js
+++ src/routes/v1/projects.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { createProjectController } from "../../controllers/projectController.js";
+import {
+  createProjectController,
+  getProjectTree,
+} from "../../controllers/projectController.js";
 
 const router = express.Router();
 
 router.post("/", createProjectController);
+
+router.get("/:projectId/tree", getProjectTree);
 
 export default router;
