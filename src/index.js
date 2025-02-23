7952cf63eba617eb81d223493578c03b91229a60
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-02-23 12:33:35 +0530

    Added socket events for writing files and more - backend

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ editorNamespace.on("connection", (socket) => {
     });
   }
 
-  handleEditorSocketEvents(socket);
+  handleEditorSocketEvents(socket, editorNamespace);
 
   // socket.on("disconnect", async () => {
   //     await watcher.close();
